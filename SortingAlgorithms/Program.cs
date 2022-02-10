724c0c024d46bacdacec69ba61fe941c4ad6654e
welamic <63785730+welamic@users.noreply.github.com>
2022-02-10 23:35:26 +0000

    Create 'SortingAlgorithms' Project

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿// using System;
+
+// namespace Algorithms
+// {
+//     public static class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello, Old World!");
+//         }
+//     }
+// }
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, New World!");
